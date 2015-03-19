d3e5016e9ef88f71d1c7f42355e16b992e5d9f38
Daniel <danielgoodnow@gmail.com>
2015-03-18 20:27:53 -0700

    use threaded i/o

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <pthread.h>
 
 using namespace std;
 
@@ -12,11 +13,21 @@ struct component {
    int cost;
 };
 
-void ReadProgram(string fname, vector<component> &components);
+struct readProgData {
+   string fname;
+   vector<component> components;
+};
+
+void *ReadProgram(void * threadArg);
 int main(int argc, char* argv[])
-{
-   vector<component> comps;
-   ReadProgram(argv[1], comps);
+{ 
+   pthread_t readThread;
+   readProgData rpd;
+   rpd.fname = argv[1];
+   int rc = pthread_create(&readThread, NULL, ReadProgram, (void *)&rpd);
+   void* status;
+   pthread_join(readThread, &status);
+   vector<component> comps = rpd.components;
    for(vector<component>::iterator it = comps.begin(); it != comps.end(); ++it)
       cout << (*it).type << '\t' << (*it).operation << '\t' << (*it).cost << '\n';
    return 0;
@@ -31,12 +42,14 @@ int main(int argc, char* argv[])
  *  P(run)12; O(hard drive)11; P(run)5; I(hard drive)12; O(hard drive)12;
  *  P(run)5; O(monitor)10; P(run)12; O(monitor)10; A(end)0; S(end)0.
  *  End Program Meta-Data Code.
- */
+*/
 
-void ReadProgram(string fname, vector<component> &components)
+void *ReadProgram(void *threadArg)
 {
+   readProgData *data;
+   data = (readProgData *) threadArg;
    ifstream metaFile;
-   metaFile.open(fname.c_str(), fstream::in);
+   metaFile.open(data->fname.c_str(), fstream::in);
    string temp;
 
    //discard start line
@@ -49,7 +62,7 @@ void ReadProgram(string fname, vector<component> &components)
       getline(metaFile, next.operation, ')');
       metaFile >> next.cost;
       metaFile.get();
-      components.push_back(next);
+      data->components.push_back(next);
    } while(!(next.type == 'S' && next.operation == "end"));
    metaFile.close();
 }
